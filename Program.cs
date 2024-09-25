26ee82ed846d90590c001bcdea8baf46f98771a4
emrevtnsvr <e.vatansever885@mybvc.ca>
2024-09-25 17:26:36 -0600

    Leaning Activity 3.1

--- Program.cs
+++ Program.cs
@@ -1,33 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-LearningActivity31();
-LearningActivity32();
-LearningActivity33();
-LearningActivity34();
-LearningActivity35();
-
-void LearningActivity31()
+﻿using System;
+class Program
 {
-    Console.WriteLine("This is the function for Learning Activity 3.1!");
+    static void Main()
+    {
+        // Input temperature in Celsius
+        Console.Write("Enter temperature in Celsius: ");
+        double celsius = Convert.ToDouble(Console.ReadLine());
+        // Conversion formula
+        double fahrenheit = (celsius * 9 / 5) + 32;
+        // Output result
+        Console.WriteLine($"Temperature in Fahrenheit: {fahrenheit}");
+    }
 }
-void LearningActivity32()
-{
-    Console.WriteLine("This is the function for Learning Activity 3.2!");
-}
-void LearningActivity33()
-{
-    Console.WriteLine("This is the function for Learning Activity 3.3!");
-}
-
-void LearningActivity34()
-{
-    int number;
-    bool success = int.TryParse(Console.ReadLine(), out number); //example code
-    Console.WriteLine("This is the function for Learning Activity 3.4!");
-}
-
-void LearningActivity35()
-{
-    int[] highScores = { 1272700, 1271100, 1243000, 1218000, 1214300, 1210800, 1210400, 1206800, 1178400 };
-    Console.WriteLine("This is the function for Learning Activity 3.5!");
-}
-
